e76fcc64c77590143fa020f8076a770ed4a3e885
TangoUniform <460642@student.fontys.nl>
2023-06-22 15:40:58 +0200

    Implemented controller v2

--- turtle_ws/install/turtlebot3_node/share/turtlebot3_node/local_setup.sh
+++ turtle_ws/install/turtlebot3_node/share/turtlebot3_node/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/mart/Documents/git/Turtlebot3_Simulation_WorkSpace/turtle_ws/install/turtlebot3_node"}
+: ${AMENT_CURRENT_PREFIX:="/home/tangouniform/Documents/cordycepsws/Turtlebot3_Simulation_WorkSpace/turtle_ws/install/turtlebot3_node"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
